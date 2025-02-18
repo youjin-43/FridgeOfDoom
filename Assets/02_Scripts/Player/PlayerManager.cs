f914c00ee529b0b75d50f6e515d46fab238c6913
김석진 <69078017+eeptmxlsl15@users.noreply.github.com>
2025-02-18 13:49:53 +0900

    풀 매니져 , 불릿 스크립트 추가

--- Assets/02_Scripts/Player/PlayerManager.cs
+++ Assets/02_Scripts/Player/PlayerManager.cs
@@ -1,3 +1,4 @@
+using Photon.Pun.Demo.Asteroids;
 using StarterAssets;
 using Unity.Cinemachine;
 using UnityEngine;
@@ -5,7 +6,11 @@ using UnityEngine;
 public class PlayerManager : MonoBehaviour
 {
     private StarterAssetsInputs input;
-
+    public GameObject bulletPrefab; // 던질 프리팹
+    public Transform bulletSpawnPoint; // 프리팹이 생성될 위치
+    public float bulletSpeed = 10f; // 프리팹 초기 속도
+    public float bulletArc = 5f; // 포물선 곡률 조정
+    public Transform cameraTransform;
     [Header("Aim")]
     [SerializeField]
     private CinemachineVirtualCamera aimCam;
@@ -25,6 +30,24 @@ public class PlayerManager : MonoBehaviour
         else
         {
             aimCam.gameObject.SetActive(false);
+        }
+        if (Input.GetKeyDown(KeyCode.F)) // 발사 입력 감지 (Fire1 버튼)
+        {
+            ThrowProjectile();
+        }
+    }
+
+    void ThrowProjectile()
+    {
+        if (bulletPrefab != null && bulletSpawnPoint != null)
+        {
+            GameObject projectile = Instantiate(bulletPrefab, bulletSpawnPoint.position, Quaternion.identity);
+            Rigidbody rb = projectile.GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                Vector3 throwDirection = cameraTransform.forward * bulletSpeed + Vector3.up * bulletArc;
+                rb.linearVelocity = throwDirection;
+            }
         }
     }
 }
