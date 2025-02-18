9217c52befb027cdca39f51a413e981a5ab098bb
hjlee5982 <hjlee5982@naver.com>
2025-02-18 23:57:57 +0900

    02_쿨타임 이펙트 추가

---
+++ Assets/77_InGameUI/02_Timer/UI_Timer.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class UI_Timer : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private int v = 0;
+
+    public void SetValue(int  value)
+    {
+        v = value;
+    }
+}
